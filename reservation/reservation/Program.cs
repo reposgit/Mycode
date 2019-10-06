7eac3861d1a4dc721d26a2eb08a68e8b3b85be0e
repositoryweb <skyoneill@ya.ru>
2019-10-06 16:07:20 +0700

    add to repository

---
+++ reservation/reservation/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data.SqlClient;
+
+namespace reservation
+{
+    class Program
+    {
+        static object locker = new object();
+        static void Main(string[] args)
+        {
+            var storage = new storage();
+            Parallel.For(1, 11, test);
+            Console.ReadLine();
+        }
+
+        static void test(int userid)
+        {
+            lock (locker)//резервировать один товар может только первый успевший поток -> блокировка от других потоков
+            {
+                storage.breakpoint = false;//разблокировка для выполнения после предыдущего потока
+                Console.WriteLine("ID потока: {0}", userid);
+                for (int i = 1; i <= 1000; i++)
+                {   
+                    if(storage.breakpoint)
+                    {
+                        break;
+                    }                
+                    if (storage.state == "Open")//если соединение открыто прерываем цикл - чтобы другой поток в это время не мог сделать изменения
+                    {
+                        break;
+                    }
+                    storage.reserve("товар", 1, userid);
+                }
+            }
+        }
+    }       
+}
---
+++ reservation/reservation/storage.cs
@@ -0,0 +1,79 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace reservation
+{
+    class storage
+    {
+        public static string state;
+        public static bool breakpoint;
+        
+        public static void reserve(string product, int amount, int userid)
+        {
+            try
+            {
+                string connectionString = ConfigurationManager.ConnectionStrings["DefaultConnection"].ConnectionString;
+                string sqlExpression = string.Format("SELECT count(*) FROM products where product='{0}'", product);                
+                using (SqlConnection connection = new SqlConnection(connectionString))
+                {
+                    connection.Open();
+                    state = connection.State.ToString();//проверка статуса соединения
+                    try
+                    {
+                        SqlCommand cmd = new SqlCommand(sqlExpression, connection);
+                        cmd.Connection = connection;
+                        Int32 count = Convert.ToInt32(cmd.ExecuteScalar());
+                        if (count == 0)//проверка есть такой товар в списке вообще или нет
+                        {
+                            Console.WriteLine("Такого товара нет в магазине");
+                            connection.Close();
+                            breakpoint = true;
+                        }
+                        else
+                        {
+                            sqlExpression = String.Format("SELECT amount from products where product='{0}'", product);//запрос количества товара на складе
+                            cmd.CommandText = sqlExpression;
+                            Int32 amountdb = Convert.ToInt32(cmd.ExecuteScalar());
+                            if (amountdb < amount)
+                            {
+                                Console.WriteLine("Количество товара на складе меньше резервируемого, резервирование не выполнено");
+                                connection.Close();
+                                breakpoint = true;
+                            }
+                            else
+                            {
+                                amountdb = amountdb - amount;//расчет остатка
+                                sqlExpression = String.Format("update [testbd].[dbo].[products] set [amount]={0}, [reservation]='process' where [product]='{1}'", amountdb, product);//обновление информации о товаре на складе (остаток и статус)
+                                cmd.CommandText = sqlExpression;
+                                cmd.ExecuteNonQuery();
+                                sqlExpression = String.Format("update [testbd].[dbo].[reserv] set [amount]=[amount]+{0}, [reservation]='process' where [product]='{1}'", amount, product);//обновление информации о резервировании (сумма резерва и статус)
+                                cmd.CommandText = sqlExpression;
+                                cmd.ExecuteNonQuery();
+                                breakpoint = false;
+                                connection.Close();
+                                Console.WriteLine("Пользователь: {0} /Зарезервировал товара '{1}': {2} шт /Остаток на складе: {3}",userid, product, amount, amountdb);                               
+                            }
+                        }
+                    }                
+                    catch (Exception ex)
+                    {
+                        Console.WriteLine($"Исключение: {ex.Message}");
+                    }
+                    finally
+                    {
+                        state = connection.State.ToString();
+                    }
+                }
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine($"Исключение: {ex.Message}");
+            }
+        }
+    }
+}
